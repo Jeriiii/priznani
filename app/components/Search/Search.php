1dc0387ed0218c34849feaa7c278dec6c23cdd79
mariokam <mario.kamburov@volny.cz>
2014-02-28 10:29:27 +0100

    search component

---
+++ app/components/Search/Search.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Search Component
+ *
+ * @author Mario
+ */
+
+class Search extends Nette\Application\UI\Control
+{     
+         
+	public function __construct()
+        {                        
+            parent::__construct();           
+	}
+
+	public function render()
+	{
+
+               
+                
+	}   
+}
+?>
