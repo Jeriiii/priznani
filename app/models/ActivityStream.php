938795697d22a0693fbdee7217e35c5d13ee68fb
mariokam <mario.kamburov@volny.cz>
2014-04-07 21:54:28 +0200

    stream výpis advices & confessions

--- app/models/ActivityStream.php
+++ app/models/ActivityStream.php
@@ -9,7 +9,25 @@ class ActivityStream extends Selection
     {
         parent::__construct('stream_items', $connection);
     }
-	
+     
+         public function addNewConfession($confessionID, $userID) {
+		$this->insert(array(
+			"confessionID" => $confessionID,
+			"userID" => $userID,
+			"type" => 1,
+			"create" => new DateTime(),
+		));
+	}
+        
+        public function addNewAdvice($adviceID, $userID) {
+		$this->insert(array(
+			"adviceID" => $adviceID,
+			"userID" => $userID,
+			"type" => 1,
+			"create" => new DateTime(),
+		));
+	}
+    
 	public function addNewGallery($userGalleryID, $userID) {
 		$this->insert(array(
 			"userGalleryID" => $userGalleryID,
