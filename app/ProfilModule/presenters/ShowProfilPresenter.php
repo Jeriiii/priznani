c8c8333a317641cb655291c4060ffee6544b964f
mariokam <mario.kamburov@volny.cz>
2014-07-05 10:19:51 +0200

    refs #pos-84, rozdělení do 3 šablon

--- app/ProfilModule/presenters/ShowProfilPresenter.php
+++ app/ProfilModule/presenters/ShowProfilPresenter.php
@@ -52,6 +52,11 @@ class ShowProfilPresenter extends ProfilBasePresenter {
         public $dataForStream;
 	private $count = 0;
 
+        /**
+         * metoda nastavuje hodnoty predavanych parametru predtim, nez se sablona s uzivatelskym streamem vykresli.
+         * Tyto hodnoty pak predava komponente Stream
+         * @param type $id
+         */
 	public function actionDefault($id) {
                 if (empty($id)) {
 			$id = $this->getUser()->getId();
@@ -59,17 +64,52 @@ class ShowProfilPresenter extends ProfilBasePresenter {
 		$this->dataForStream = $this->streamDao->getUserStreamPosts($id);
 		$this->count = $this->dataForStream->count("id");
 	}
-        
-        
+                
+        /**
+         * vykresluje  uzivatelsky stream (zed s vlastnimi prispevky)
+         * @param type $id
+         */
 	public function renderDefault($id) {
 		if (empty($id)) {
+			$id = $this->getUser()->getId();
+		}
+
+		$this->userID = $id;		
+		$user = $this->getUserBaseInfo($id);
+
+		$this->template->userData = $user;
+                $this->template->userID = $id;
+                $this->template->count = $this->count;
+	}
+        
+        /**
+         * vykresluje template se vsemi fotky uzivateli
+         * @param type $id
+         */
+        public function renderUserImages($id){
+                if (empty($id)) {
+			$id = $this->getUser()->getId();
+		}
+		$user = $this->getUserBaseInfo($id);
+		$this->userID = $id;
+                
+		$this->template->userData = $user;
+		$this->template->userID = $id;
+        }
+        
+        /**
+         * vykresluje informace uzivatele, pripadne partnerovi
+         * @param type $id
+         */
+        public function renderUserInfo($id){
+            	if (empty($id)) {
 			$id = $this->getUser()->getId();
 		}
 
 		$this->userID = $id;
 		$this->template->userID = $id;
 
-		$user = $this->userDao->find($id);
+		$user = $this->getUserBaseInfo($id);
 		$userProperty = $this->userDao->findProperties($id);
 
 		$this->template->userData = $user;
@@ -79,14 +119,29 @@ class ShowProfilPresenter extends ProfilBasePresenter {
 		if ($property == 'couple' || $property == 'coupleMan' || $property == 'coupleWoman') {
 			$this->template->userPartnerProfile = $this->coupleDao->getPartnerData($userProperty->id_couple);
 		}
-                               
-                $this->template->count = $this->count;
-	}
-
+        }
+        
+        /**
+         * vrati zakladni uzivatelske informace z tabulky users jako napr. user_name nebo email apod.
+         * @param type $id - user id
+         * @return type
+         */
+        private function getUserBaseInfo($id){
+            return $this->userDao->find($id);
+        }
+        
+        /**
+         * Vykresluje uzivatelsky stream, respektive jeho prispevky k hlavnemu streamu
+         * @return \Stream
+         */
 	protected function createComponentStream() {
 		return new \Stream($this->dataForStream, $this->streamDao, $this->userGalleryDao, $this->userImageDao, $this->confessionDao);
 	}
         
+        /**
+         * vykresluje vsechny galerie daneho uzivatele
+         * @return \POSComponent\Galleries\UserGalleries\UserGalleries
+         */
 	public function createComponentUserGalleries() {
 		return new UserGalleries($this->userDao, $this->userGalleryDao);
 	}
@@ -100,6 +155,11 @@ class ShowProfilPresenter extends ProfilBasePresenter {
 
 		return new UserImagesInGallery($gallery->id, $images, $this->userDao);
 	}
+        
+        /**
+         * WebLoader pro minifikace skriptu
+         * @return \WebLoader\Nette\JavaScriptLoader
+         */
 	public function createComponentJs() {
 		$files = new \WebLoader\FileCollection(WWW_DIR . '/js');
 		$files->addFiles(array(
