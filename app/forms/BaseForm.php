9355461a58e507c8c10d722788e7816b42f177ab
mariokam <mario.kamburov@volny.cz>
2014-07-07 13:42:30 +0200

    refaktoring a čistka metod v BaseForm třídach

--- app/forms/BaseForm.php
+++ app/forms/BaseForm.php
@@ -20,44 +20,4 @@ class BaseForm extends Form {
 		parent::__construct($parent, $name);
 	}
 
-	/**
-	 * odeslani zpravy o odeslani formulare
-	 */
-	public function sendMailAboutSendForm($form_name, $presenter) {
-		$users = $presenter->context->createUsers()
-			->where("form_mail", 1);
-
-//		foreach($users as $user)
-//		{
-//			$mail = new Message;
-//			$mail->setFrom('info@nejlevnejsiwebstranky.cz')
-//				->addTo($user->mail)
-//				->setSubject('upozornění - odpověď ve formulář')
-//				->setBody("Dobrý den,\n\n posíláme upozornění, že byl vyplněn a odeslán formulář " . $form_name . " na Vašich stránkách. \n\n Tým nejlevnejšíwebstránky")
-//				->send();
-//		}
-	}
-
-	/**
-	 * zaznamenani odeslani mailu do specialni tabulky
-	 */
-	public function registerNewSendForm($path, $id_form, $presenter, $name, $id_click) {
-		if ($path == "standart") {
-			$form = $presenter->context->createForms()
-				->find($id_form)
-				->fetch();
-			$link = $presenter->link("Admin:Forms:formsX", array("id_form" => $form->id, "type" => $form->type, "id_click" => $id_click->id));
-			$name = $form->name;
-		} else {
-			$link = $presenter->link($path, array("id_click" => $id_click->id));
-//			echo $path . " - "; echo $id_click . " - "; die($link);
-		}
-		$presenter->context->createForm_new_send()
-			->insert(array(
-				"path" => $link,
-				"name" => $name,
-				"date" => new DateTime()
-		));
-	}
-
 }
