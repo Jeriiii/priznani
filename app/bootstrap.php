7528e68e0bf7170bcc86045d74dce531a4bf2287
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-07-13 14:25:00 +0200

    zablokování rout na develop

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -44,41 +44,41 @@ $router = new RouteList;
 
 $container->router = $router;
 $router[] = new Route('index.php', 'OnePage:default', Route::ONE_WAY);
-$router[] = new Route('//[www.]priznanizparty.cz/[/<presenter>/<url>]', array(
-	'presenter' => 'Page',
-	'action' => 'default',
-	'url' => 'priznani-z-party'
-	));
-$router[] = new Route('//[www.]priznanizparby.cz/[/<presenter>/<url>]', array(
-	'presenter' => 'Page',
-	'action' => 'default',
-	'url' => 'priznanizparby'
-	));
-$router[] = new Route('//priznaniosexu.cz/seznamka[/<presenter>/<url>]', array(
-	'presenter' => 'Page',
-	'action' => 'default',
-	'url' => 'seznamka'
-	));
-$router[] = new Route('//priznaniosexu.cz/[/<presenter>/<url>]', array(
-	'presenter' => 'Page',
-	'action' => 'default',
-	'url' => 'priznani-o-sexu'
-	));
-$router[] = new Route('//priznaniosexu.cz/poradna/[/<presenter>/<url>]', array(
-	'presenter' => 'Page',
-	'action' => 'default',
-	'url' => 'poradna-o-sexu'
-	));
-$router[] = new Route('//priznaniosexu.cz/priznani/<id>', array(
-	'presenter' => 'Page',
-	'action' => 'confession',
-	'id' => '<id>'
-	));
-$router[] = new Route('//priznaniosexu.cz/poradna/<id>', array(
-	'presenter' => 'Page',
-	'action' => 'advice',
-	'id' => '<id>'
-	));
+//$router[] = new Route('//[www.]priznanizparty.cz/[/<presenter>/<url>]', array(
+//	'presenter' => 'Page',
+//	'action' => 'default',
+//	'url' => 'priznani-z-party'
+//	));
+//$router[] = new Route('//[www.]priznanizparby.cz/[/<presenter>/<url>]', array(
+//	'presenter' => 'Page',
+//	'action' => 'default',
+//	'url' => 'priznanizparby'
+//	));
+//$router[] = new Route('//priznaniosexu.cz/seznamka[/<presenter>/<url>]', array(
+//	'presenter' => 'Page',
+//	'action' => 'default',
+//	'url' => 'seznamka'
+//	));
+//$router[] = new Route('//priznaniosexu.cz/[/<presenter>/<url>]', array(
+//	'presenter' => 'Page',
+//	'action' => 'default',
+//	'url' => 'priznani-o-sexu'
+//	));
+//$router[] = new Route('//priznaniosexu.cz/poradna/[/<presenter>/<url>]', array(
+//	'presenter' => 'Page',
+//	'action' => 'default',
+//	'url' => 'poradna-o-sexu'
+//	));
+//$router[] = new Route('//priznaniosexu.cz/priznani/<id>', array(
+//	'presenter' => 'Page',
+//	'action' => 'confession',
+//	'id' => '<id>'
+//	));
+//$router[] = new Route('//priznaniosexu.cz/poradna/<id>', array(
+//	'presenter' => 'Page',
+//	'action' => 'advice',
+//	'id' => '<id>'
+//	));
 $router[] = new Route('<presenter>/<action>[/<url>]', 'OnePage:default');
 
 Container::extensionMethod('addDateTimePicker', function (Container $_this, $name, $label, $cols = NULL, $maxLength = NULL) {
