30037ac2721da2508c5cd4f09d833692f7590db5
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-07-16 19:48:19 +0200

    odstranění hesla z hlavní formuláře, ať se nemusí pořád zadávat. Rozdělení formuláře pro editaci na dva (info, foto)

--- app/ProfilModule/forms/DatingEditFirstForm.php
+++ app/ProfilModule/forms/DatingEditFirstForm.php
@@ -5,50 +5,50 @@ namespace Nette\Application\UI\Form;
 use Nette\Application\UI\Form,
 	Nette\Security as NS,
 	Nette\ComponentModel\IContainer,
-        POS\Model\UserDao,
-        POS\Model\UserPropertyDao;
+	POS\Model\UserDao,
+	POS\Model\UserPropertyDao;
 
 class DatingEditFirstForm extends DatingRegistrationFirstForm {
 
-        /**
+	/**
 	 * @var \POS\Model\UserDao
 	 */
 	public $userDao;
-        
-        /**
+
+	/**
 	 * @var \POS\Model\UserPropertyDao
 	 */
-        public $userPropertyDao;
+	public $userPropertyDao;
 	private $id_user;
 
 	public function __construct(UserPropertyDao $userPropertyDao, UserDao $userDao, IContainer $parent = NULL, $name = NULL) {
 		parent::__construct($userDao, $parent, $name);
 
-                $this->userPropertyDao = $userPropertyDao;
+		$this->userPropertyDao = $userPropertyDao;
 		$presenter = $this->getPresenter();
 		$this->id_user = $presenter->getUser()->getId();
 		$userInfo = $this->userDao->find($this->id_user);
 
-                /* $this["age"]->setDefaultValue($userInfo->property->age); */
-                
-                $this["user_property"]->setDefaultValue($userInfo->property->user_property);
-                $this["interested_in"]->setDefaultValue($userInfo->property->interested_in);
-                
-                $this["send"]->caption = "Uložit";
-                $this["send"]->setAttribute("class", "btn btn-info");
-                
+		/* $this["age"]->setDefaultValue($userInfo->property->age); */
+
+		$this["user_property"]->setDefaultValue($userInfo->property->user_property);
+		$this["interested_in"]->setDefaultValue($userInfo->property->interested_in);
+
+		$this["send"]->caption = "Uložit";
+		$this["send"]->setAttribute("class", "btn btn-info");
+
 		return $this;
 	}
 
 	public function submitted($form) {
 		$values = $form->values;
 		$presenter = $this->getPresenter();
-                $this->id_user = $presenter->getUser()->getId();
+		$this->id_user = $presenter->getUser()->getId();
 		$record = $this->userDao->find($this->id_user);
-                if (!$record) {
+		if (!$record) {
 			throw new BadRequestException;
 		}
-                $this->userPropertyDao->update($record->propertyID, array(/*'age' => $values->age, */'user_property' => $values->user_property, 'interested_in' => $values->interested_in));
+		$this->userPropertyDao->update($record->propertyID, array(/* 'age' => $values->age, */'user_property' => $values->user_property, 'interested_in' => $values->interested_in));
 		$presenter->redirect('this');
 	}
 
--- app/ProfilModule/forms/DatingEditSecondForm.php
+++ app/ProfilModule/forms/DatingEditSecondForm.php
@@ -5,63 +5,67 @@ namespace Nette\Application\UI\Form;
 use Nette\Application\UI\Form,
 	Nette\Security as NS,
 	Nette\ComponentModel\IContainer,
-        POS\Model\UserDao,
-        POS\Model\UserPropertyDao;
+	POS\Model\UserDao,
+	POS\Model\UserPropertyDao;
+use Nette\Database\Table\ActiveRow;
 
-class DatingEditSecondForm extends DatingRegistrationSecondForm {
+class DatingEditSecondForm extends BaseForm {
 
-        /**
-	 * @var \POS\Model\UserDao
-	 */
+	/** @var \POS\Model\UserDao */
 	public $userDao;
-        /**
-	 * @var \POS\Model\UserPropertyDao
-	 */
-        public $userPropertyDao;
-	private $id_user;
-	private $record;
+
+	/** @var \POS\Model\UserPropertyDao */
+	public $userPropertyDao;
+
+	/** @var \Nette\Database\Table\ActiveRow */
+	private $user;
 
 	public function __construct(UserPropertyDao $userPropertyDao, UserDao $userDao, IContainer $parent = NULL, $name = NULL) {
-		parent::__construct($userDao, $parent, $name);
+		parent::__construct($parent, $name);
 
-                $this->userPropertyDao = $userPropertyDao;
+		$this->userPropertyDao = $userPropertyDao;
 		$presenter = $this->getPresenter();
-		$this->id_user = $presenter->getUser()->getId();
+		$userID = $presenter->getUser()->getId();
 
-		$userInfo = $this->userDao->find($this->id_user);
+		$this->user = $userDao->find($userID);
 
 		$this->addGroup('Identifikační údaje');
-                $email = $userInfo->email;
-                $this["email"]->setDisabled();
-			
-                $this["user_name"]->setDisabled();
-                
-                
-                $this["first_sentence"]->setDefaultValue($userInfo->property->first_sentence);
 
-                $this["about_me"]->setDefaultValue($userInfo->property->about_me);
-                
-                $this["send"]->caption = "Uložit";
-                $this["send"]->setAttribute("class", "btn btn-info");
+		$this->addText('email', 'Email')
+			->addRule(Form::FILLED, 'Email není vyplněn.')
+			->addRule(Form::EMAIL, 'Vyplněný email není platného formátu.')
+			->addRule(Form::MAX_LENGTH, 'Email je příliž dlouhý.', 50)
+			->setDisabled();
+		$this->addText('user_name', 'Uživatelské jméno')
+			->addRule(Form::FILLED, 'Uživatelské jméno není vyplněno')
+			->addRule(Form::MAX_LENGTH, 'Maximální délka pole \"Uživatelské jméno\" je 100 znaků.', 20)
+			->setDisabled();
+		$this->addText('first_sentence', 'Úvodní věta (max 100 znaků)')
+			->addRule(Form::FILLED, 'Úvodní věta není vyplněna.')
+			->addRule(Form::MAX_LENGTH, 'Maximální délka pole \"Úvodní věta\" je 100 znaků.', 100);
+		$this->addTextArea('about_me', 'O mě (max 300 znaků)', 40, 10)
+			->addRule(Form::FILLED, 'O mě není vyplněno.')
+			->addRule(Form::MAX_LENGTH, 'Maximální délka pole \"O mě\" je 300 znaků.', 300);
+
+		$this->setDefaults(array(
+			"email" => $this->user->email,
+			"user_name" => $this->user->user_name,
+			"first_sentence" => $this->user->property->first_sentence,
+			"about_me" => $this->user->property->about_me
+		));
+
+		$this->onSuccess[] = callback($this, 'submitted');
+		$this->addSubmit('send', 'Uložit')
+			->setAttribute("class", "btn btn-info");
 
 		return $this;
 	}
 
 	public function submitted($form) {
 		$values = $form->values;
-
 		$presenter = $this->getPresenter();
-		$this->id_user = $presenter->getUser()->getId();
 
-		$this->record = $this->userDao->find($this->id_user);
-		if (!$this->record) {
-			throw new BadRequestException;
-		}
-		$authenticator = $presenter->context->authenticator;
-		$pass = $authenticator->calculateHash($values->password);
-
-		$this->userDao->update($this->id_user, array(/* 'email' => $email, 'user_name' => $user_name, */'password' => $pass));
-                $this->userPropertyDao->update($this->record->propertyID, array('first_sentence' => $values->first_sentence, 'about_me' => $values->about_me));
+		$this->userPropertyDao->update($this->user->propertyID, array('first_sentence' => $values->first_sentence, 'about_me' => $values->about_me));
 		$presenter->flashMessage('Změna identifikačních údajů byla úspěšná');
 		$presenter->redirect("this");
 	}
