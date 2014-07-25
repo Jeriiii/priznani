afa4ea63fcc419cb5bf1076515bf06e9802c3fd9
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-07-25 11:46:01 +0200

    vytvoření nové stable databáze

---
+++ database/path-2014-07-25/parts/08-cleaning.sql
@@ -0,0 +1,17 @@
+/* smazání nepotřebných tabulek */
+DROP TABLE `facebook`;
+DROP TABLE `wall_items`;
+DROP TABLE `texts`;
+DROP TABLE `pages_galleries`;
+DROP TABLE `pages_forms`;
+DROP TABLE `pages`;
+DROP TABLE `map`;
+DROP TABLE `forms_query`;
+DROP TABLE `forms`;
+DROP TABLE `forms3`;
+DROP TABLE `forms2`;
+DROP TABLE `form_new_send`;
+DROP TABLE `news`;
+DROP TABLE `news_galleries`;
+DROP TABLE `google_analytics`;
+DROP TABLE `authorizator_table`;
