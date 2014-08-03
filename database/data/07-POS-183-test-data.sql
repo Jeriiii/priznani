37d30ff24eea327d61fa5c9d4ff908119cd6e80f
Daniel Holubář <daniel.holubar@gmail.com>
2014-08-03 23:14:21 +0200

    oprava scriptu pro test data, testy na upload a schvaleni soutěžních obrázků

--- database/data/07-POS-183-test-data.sql
+++ database/data/07-POS-183-test-data.sql
@@ -1,7 +1,7 @@
 /* Vloží testovací soutěž */
-INSERT INTO `postest`.`users_competitions` (`name`, `description`) VALUES ('Test competition', 'Test competition');
+INSERT INTO `users_competitions` (`name`, `description`, `imageUrl`) VALUES ('Test competition', 'Test competition', 'images/galleries/5/79.JPG');
 /* Získá id poslední vložené soutěže, potřebné pro FK v dolní části */
 SELECT @last_id := MAX(id) FROM `postest`.`users_competitions`;
 /* Vloží testovací obrázky pro testovací soutěž */
-INSERT INTO `postest`.`competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (3, 3, @last_id, 1);
-INSERT INTO `postest`.`competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (2, 3, @last_id, 1);
+INSERT INTO `competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (3, 3, @last_id, 1);
+INSERT INTO `competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (2, 3, @last_id, 1);
