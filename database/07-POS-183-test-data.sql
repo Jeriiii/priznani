c0e10ac9419e6400db186f57be45e4ea9ba4f182
Daniel Holubář <daniel.holubar@gmail.com>
2014-08-03 22:08:09 +0200

    Opravený datový script s test daty

--- database/07-POS-183-test-data.sql
+++
@@ -1,5 +0,0 @@
-/* Vloží testovací soutěž */
-/*INSERT INTO `postest`.`users_competitions` (`id`, `name`, `description`) VALUES ('1', 'Test competition', 'Test competition');
-/* Vloží testovací obrázky pro testovací soutěž */
-/*INSERT INTO `postest`.`competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (3, 3, 1, 1);
-INSERT INTO `postest`.`competitions_images` (`imageID`, `userID`, `competitionID`, `allowed`) VALUES (2, 3, 1, 1);*/
