b6e9e45b8d5be365dc6155c6693dc25e5ab8fdc9
JKoty <jan.kotalik@email.cz>
2014-08-07 16:04:26 +0200

    sql data do tabulky

---
+++ database/data/08-POS-154-contacts.sql
@@ -0,0 +1,12 @@
+
+-- Dumping data for table pos.chat_contacts: ~4 rows (approximately)
+/*!40000 ALTER TABLE `chat_contacts` DISABLE KEYS */;
+INSERT INTO `chat_contacts` (`id`, `id_user`, `id_contact`) VALUES
+	(1, 87, 86),
+	(2, 86, 1),
+	(3, 1, 86),
+	(4, 1, 87),
+	(5, 86, 87);
+/*!40000 ALTER TABLE `chat_contacts` ENABLE KEYS */;
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
