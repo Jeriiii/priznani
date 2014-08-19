cf02bf3f1bdf8126efb5c184f98f302771719817
JKoty <jan.kotalik@email.cz>
2014-08-19 11:54:51 +0200

    styly, obrázky

--- www/js/activities.js
+++ www/js/activities.js
@@ -8,7 +8,7 @@ var clearUrl = removeFid(baseUrl, index);
 var url = clearUrl + askUrl;
 
 //Dotaz nanové aktivity při reloadu(tak jako na fb)
-$(document).ready(function(){
+$(document).ready(function() {
 	getNewActivitiesCount();
 });
 
@@ -23,7 +23,7 @@ function getNewActivitiesCount() {
 		dataType: "json",
 		async: true,
 		success: function(response) {
-			
+
 		},
 		complete: function(payload) {
 			//Získání části JSONu s daty
@@ -31,12 +31,12 @@ function getNewActivitiesCount() {
 			//Rozparsování
 			var obj = JSON.parse(json);
 			//Pokud už bylo počítadlo vykresleno, smažeme
-			if('.new-counter') {
+			if ('.new-counter') {
 				$("#activities .new-counter").remove();
 			}
-			
+
 			//Pokud jsou nové příspěvky, tak ukážeme kolik
-			if(obj.count > 0) {
+			if (obj.count > 0) {
 				$(".activities-btn").append('<div class="new-counter">' + obj.count + '</div>');
 			}
 		}
@@ -44,14 +44,14 @@ function getNewActivitiesCount() {
 }
 
 // div, který překreje tlačítko při ukázání aktivit, a který pak aktivity zavře
-var close='<div class="closer"></div>';
+var close = '<div class="closer"></div>';
 var loading = $('#loadingDiv');
 var activitiesBtn = $('.activities-btn');
 var activitiesDrop = $('#activities-droplink');
 
 //Pokud se klikne mimo okno aktivit, zavře ho a odstraní zavíraci div
 //Je immuni, když se kliká na zavírací element, protože ten si obstará zavření sám
-$(document).mouseup(function (e)
+$(document).mouseup(function(e)
 {
 	if (!activitiesDrop.is(e.target) && activitiesDrop.has(e.target).length === 0 && !$('.closer').is(e.target))
 	{
@@ -61,22 +61,22 @@ $(document).mouseup(function (e)
 });
 
 //Zavře okno s aktivitama a smaže zavírací div
-$(document).on("click", '.closer', function(){
+$(document).on("click", '.closer', function() {
 	activitiesDrop.fadeOut();
 	$('#activities .closer').remove();
 });
 
 //Při ajaxovém načítání ukáže spinner a upraví okno aktivit
-$(document).ajaxStart(function () {
-	activitiesDrop.height(60).css('overflow-y', 'hidden');
+$(document).ajaxStart(function() {
+	//activitiesDrop.height(60).css('overflow-y', 'hidden');//zakomentovano od Kotyho kvuli stylum
 	loading.show();
-  })
-  .ajaxStop(function () {
-	activitiesDrop.height(150).css('overflow-y', 'scroll');
-	loading.hide();
-  });
-// Při kliknutí na jednu aktivitu složí url a odešle ajax pro označení jako přečtená. 
-$(document).on("click", '#activities-droplink a', function(){
+})
+		.ajaxStop(function() {
+			//activitiesDrop.height(150).css('overflow-y', 'scroll');//zakomentovano od Kotyho kvuli stylum
+			loading.hide();
+		});
+// Při kliknutí na jednu aktivitu složí url a odešle ajax pro označení jako přečtená.
+$(document).on("click", '#activities-droplink a', function() {
 	//Získání id aktivity
 	var activityID = $(this).attr('data-activity');
 	//Část url volání handleru
@@ -87,45 +87,45 @@ $(document).on("click", '#activities-droplink a', function(){
 		url: wholeUrl,
 		async: true,
 		success: function(response) {
-			
+
 		},
 		complete: function(payload) {
-			
+
 		}
 	});
 });
 
 //Vyvolá revalidaci komponenty po označení všech odpovědí jako přečtených
-$(document).on("click", '.marker', function(){
+$(document).on("click", '.marker', function() {
 	var requestUrl = "?do=activities-loadActivities";
 	var wholeUrl = baseUrl + requestUrl;
 	$.nette.ajax({
 		url: wholeUrl,
 		async: true,
 		success: function(response) {
-			
+
 		},
 		complete: function(payload) {
-			
+
 		}
 	});
 });
 
 //funkce volá ajax, a povolí načtení každé 3s při kliku
-$(document).on("click", ".activities-btn", function(e){
+$(document).on("click", ".activities-btn", function(e) {
 	var requestUrl = "?do=activities-loadActivities";
 	var index = baseUrl.indexOf('?_fid=');
 	var clearUrl = removeFid(baseUrl, index);
 	var ajaxUrl = clearUrl + requestUrl;
 	var target = $(e.target);
 	var targetAttr = target.attr('data-ajax-off');
-	
+
 	//Otevře okno s aktivitama
 	activitiesDrop.fadeIn();
 	$("a.activities-btn").before(close);
-	
+
 	//Obstarává poslání ajax dotazu na vykreslení aktivit, v případě kliku v intervalu 3s nenačte nové
-	if(targetAttr !== '1') {
+	if (targetAttr !== '1') {
 		$('#activities .new-counter').remove();
 		$.nette.ajax({
 			url: ajaxUrl,
@@ -135,18 +135,18 @@ $(document).on("click", ".activities-btn", function(e){
 			},
 			complete: function(payload) {
 				var data = JSON.parse(payload.responseText);
-				
+
 				//Pokud uživatel nemá žádnou událost, vypíše se mu to.
-				if(data.activities === 0) {
+				if (data.activities === 0) {
 					$('#activities .marker').remove();
-					
+
 					//Kontrola, zda div s oznámením již nebyl jednou vykreslen
-					if($('.no-activities').length === 0) {
+					if ($('.no-activities').length === 0) {
 						$('#loadingDiv').after('<div class="no-activities">Zatím nemáte žádné události.</div>');
 					}
 				} else {
 					//Promazání příspěvků, aby se stále neopakovaly
-					$('#activities-droplink .item').each(function(){
+					$('#activities-droplink .item').each(function() {
 						$(this).remove();
 					});
 					//Vypsání každé události
@@ -160,15 +160,15 @@ $(document).on("click", ".activities-btn", function(e){
 			}
 		});
 		//Nastaví time na 3s, kvůli zamezení posílání ajaxu kadým klikem v rychlém sledu
-		setTimeout(function(){
+		setTimeout(function() {
 			target.removeAttr('data-ajax-off');
-		 }, 3000);
+		}, 3000);
 
 	}
 });
 
 function removeFid(url, pos) {
-	if(pos !== -1) {
+	if (pos !== -1) {
 		url = url.substr(0, pos);
 	}
 
