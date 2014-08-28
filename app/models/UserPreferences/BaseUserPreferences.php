50c499e3df35c5f6588118f16e41de572bf57df9
JKoty <jan.kotalik@email.cz>
2014-08-28 10:49:27 +0200

    vykreslení testovacího streamu 2

--- app/models/UserPreferences/BaseUserPreferences.php
+++ app/models/UserPreferences/BaseUserPreferences.php
@@ -13,8 +13,6 @@
 namespace POS\UserPreferences;
 
 use POS\Model\UserDao;
-use POS\Model\StreamDao;
-use POS\Model\StreamCategoriesDao;
 use Nette\Database\Table\ActiveRow;
 use Nette\Database\Table\Selection;
 use Nette\Caching\Cache;
@@ -26,43 +24,24 @@ class BaseUserPreferences {
 	/** @var \POS\Model\UserDao */
 	protected $userDao;
 
-	/** @var \POS\Model\StreamDao */
-	protected $streamDao;
-
-	/** @var \POS\Model\StreamCategoriesDao */
-	protected $streamCategoriesDao;
-
 	/** @var ActiveRow Vlastnosti přihlášeného uživatele. */
 	protected $userProperty;
 
 	/** @var array Nejlepší uživatelé pro tohoto uživatele */
 	protected $bestUsers;
 
-	/** @var array Nejlepší příspěvky pro tohoto uživatele */
-	protected $bestStreamItems;
-
 	/** @var Nette\Http\SessionSection Sečna do které se ukládá uživatelské vyhledávání */
 	protected $section;
 
-	/** @var Nette\Http\SessionSection Sečna do které se ukládá stav příspěvků na streamu */
-	protected $streamSection;
-
 	const NAME_SESSION_BEST_U = "bestUsers";
-	const NAME_SESSION_BEST_STREAM_ITEMS = "bestStreamItems";
 
-	public function __construct(ActiveRow $userProperty, UserDao $userDao, StreamDao $streamDao, StreamCategoriesDao $streamCategoriesDao, Session $session) {
+	public function __construct(ActiveRow $userProperty, UserDao $userDao, Session $session) {
 		$this->userProperty = $userProperty;
 		$this->userDao = $userDao;
-		$this->streamDao = $streamDao;
-		$this->streamCategoriesDao = $streamCategoriesDao;
 		$this->bestUsers = NULL;
-		$this->bestStreamItems = NULL;
 
 		$this->section = $session->getSection(self::NAME_SESSION_BEST_U);
 		$this->section->setExpiration("45 min");
-
-		$this->streamSection = $session->getSection(self::NAME_SESSION_BEST_STREAM_ITEMS);
-		$this->streamSection->setExpiration("45 min");
 	}
 
 }
--- app/models/UserPreferences/StreamUserPreferences.php
+++ app/models/UserPreferences/StreamUserPreferences.php
@@ -13,10 +13,50 @@
 namespace POS\UserPreferences;
 
 use POS\Model\UserPropertyDao;
+use Nette\Database\Table\ActiveRow;
+use POS\Model\UserDao;
+use POS\Model\StreamDao;
+use POS\Model\StreamCategoriesDao;
+use Nette\Http\Session;
 
+/**
+ * Vybere do streamu nejvhodnější data vzhledem k preferencím daného uživatele
+ */
 class StreamUserPreferences extends BaseUserPreferences implements IUserPreferences {
 
+	/**
+	 * Kolik dat vytvoří na začátku (počet řádků)
+	 */
 	const INIT_ITEMS_COUNT = 6;
+
+	/**
+	 * Název sekce v session, kterou používá pro ukládání
+	 */
+	const NAME_SESSION_BEST_STREAM_ITEMS = "bestStreamItems";
+
+	/** @var array Nejlepší příspěvky pro tohoto uživatele */
+	protected $bestStreamItems;
+
+	/** @var Nette\Http\SessionSection Sečna do které se ukládá stav příspěvků na streamu */
+	protected $streamSection;
+
+	/** @var \POS\Model\StreamDao */
+	protected $streamDao;
+
+	/** @var \POS\Model\StreamCategoriesDao */
+	protected $streamCategoriesDao;
+
+	public function __construct(ActiveRow $userProperty, UserDao $userDao, StreamDao $streamDao, StreamCategoriesDao $streamCategoriesDao, Session $session) {
+		parent::__construct($userProperty, $userDao, $session);
+		$this->streamDao = $streamDao;
+		$this->streamCategoriesDao = $streamCategoriesDao;
+
+		$this->bestStreamItems = NULL;
+
+
+		$this->streamSection = $session->getSection(self::NAME_SESSION_BEST_STREAM_ITEMS);
+		$this->streamSection->setExpiration("45 min");
+	}
 
 	/**
 	 * Přepočítá výsledky hledání uložené v session. Volá se i v případě,
