d76cc7eb199e518e80ac8cc3fe953063f3ca6105
baierova <christinetka@email.cz>
2014-09-16 19:16:16 +0200

    přidána tabulka pro oblíbená místa k sexu

---
+++ database/data/07-POS-320-enum-places.sql
@@ -0,0 +1,5 @@
+INSERT INTO `enum_place` (`place`) VALUES ('postel');
+INSERT INTO `enum_place` (`place`) VALUES ('auto');
+INSERT INTO `enum_place` (`place`) VALUES ('příroda');
+INSERT INTO `enum_place` (`place`) VALUES ('neobvyklé místo');
+INSERT INTO `enum_place` (`place`) VALUES ('na veřejnosti');
