db63fdcc4d6f5b2cb86f11fd809c3fcb030f8c95
JKoty <jan.kotalik@email.cz>
2014-10-17 16:21:23 +0200

    funkční příklad (bez podpory v dotčených souborech), zobrazuje se v debug baru

--- app/listeners/FooListener.php
+++ app/listeners/FooListener.php
@@ -11,25 +11,31 @@
  */
 class FooListener extends \Nette\Object implements \Kdyby\Events\Subscriber {
 
-	private $mailer;
+	private $dao;
 
-	public function __construct() {
+	public function __construct(\POS\Model\ChatMessagesDao $dao) {
+		$dao->setMessageReaded(54, FALSE);
+		$this->dao = $dao;
 		//$this->mailer = $mailer;
 	}
 
+	public function onPoo() {
+		$this->dao->setMessageReaded(53, FALSE);
+	}
+
+	public function onStartup() {
+		$this->dao->setMessageReaded(52, FALSE);
+	}
+
+	public function process() {
+		$this->dao->setMessageReaded(50, FALSE);
+	}
+
 	public function getSubscribedEvents() {
-		return array('Nette\Application\Application::onStartup');
+
+		return array('Nette\Application\Application::onStartup',
+			'\POS\Model\FriendDao::onMess' => 'process',
+			'BasePresenter::onPoo');
 	}
 
-	public function onStartup(Application $app) {
-		$myfile = fopen("testfile2.txt", "w");
-	}
-
-//	public function render() {
-//		$myfile = fopen("testfile2.txt", "w");
-//	}
-//
-//	public function getSubscribedEvents() {
-//		return array('POSComponent\Chat\PosChat::render');
-//	}
 }
