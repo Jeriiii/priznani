62051d810f9a60f0345a0382eb747c4b4613470d
JKoty <jan.kotalik@email.cz>
2014-10-17 12:50:02 +0200

    příklad a jeho konfigurace

---
+++ app/listeners/FooListener.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * @copyright Copyright (c) 2013-2013 Kukral COMPANY s.r.o.
+ */
+
+/**
+ * Description of FooListener
+ *
+ * @author Jan Kotalík <jan.kotalik.pro@gmail.com>
+ */
+class FooListener extends \Nette\Object implements \Kdyby\Events\Subscriber {
+	
+}
