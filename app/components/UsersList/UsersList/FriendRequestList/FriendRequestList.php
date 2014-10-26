e1b8bf1a3882709554d4dfde86da48db24760704
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-10-26 12:57:19 +0100

    Přidání vyskakovacího okénka na žádosti o přátelství.

--- app/components/UsersList/UsersList/FriendRequestList/FriendRequestList.php
+++ app/components/UsersList/UsersList/FriendRequestList/FriendRequestList.php
@@ -12,9 +12,9 @@
 
 namespace POSComponent\UsersList;
 
-use IStream;
+use IAjaxBox;
 
-class FriendRequestList extends UsersList implements \IStream {
+class FriendRequestList extends UsersList implements \IAjaxBox {
 
 	/** @var \POS\Model\FriendRequestDao */
 	private $friendRequestDao;
@@ -24,6 +24,9 @@ class FriendRequestList extends UsersList implements \IStream {
 
 	/** @var int Posun příspěvků při rolování */
 	private $offset = 0;
+
+	/** @var int Limit na jedno načtení */
+	private $limit;
 
 	public function __construct($friendRequestDao, $loggedUserID, $parent, $name) {
 		parent::__construct($parent, $name);
@@ -35,7 +38,6 @@ class FriendRequestList extends UsersList implements \IStream {
 	 * Vykresli šablonu.
 	 */
 	public function render() {
-		$this->setData($this->offset);
 		$this->renderTemplate(dirname(__FILE__) . '/' . 'friendRequestList.latte');
 	}
 
@@ -63,12 +65,15 @@ class FriendRequestList extends UsersList implements \IStream {
 	 * Tuto metodu zavolejte ze metody render. Nastavý data, který se mají vrátit v ajaxovém i normálním
 	 * požadavku v závislosti na předaném offsetu (posunu od shora).
 	 * @param int $offset Offset předaný metodou handleGetMoreData. Při vyrendrování komponenty je nula.
+	 * @param int $limit Limit na jedno načtení
 	 */
-	public function handleGetMoreData($offset) {
+	public function handleGetMoreData($offset, $limit) {
 		$this->offset = $offset;
+		$this->limit = $limit;
+		$this->setData($this->offset);
 
 		if ($this->presenter->isAjax()) {
-			$this->invalidateControl('posts');
+			$this->invalidateControl("requests");
 		} else {
 			$this->redirect('this');
 		}
@@ -79,8 +84,7 @@ class FriendRequestList extends UsersList implements \IStream {
 	 * @param int $offset O kolik příspěvků se mám při načítání dalších příspěvků z DB posunout.
 	 */
 	public function setData($offset) {
-		$limit = 4;
-		$friendRequests = $this->friendRequestDao->getAllToUser($this->loggedUserID, $limit, $offset);
+		$friendRequests = $this->friendRequestDao->getAllToUser($this->loggedUserID, $this->limit, $offset);
 		$this->template->friendRequests = $friendRequests;
 	}
 
