b84b7fef1d54a591dc1267d8486edfca7bf0b947
Petr Kukrál <P.Kukral@seznam.cz>
2014-11-04 21:19:48 +0100

    základní šablona na js

---
+++ www/js/templateJQuery.js
@@ -0,0 +1,15 @@
+<#assign licenseFirst = "/* ">
+<#assign licensePrefix = " * ">
+<#assign licenseLast = " */">
+<#include "${project.licensePath}">
+ 
+/**
+ * Description of ${name}
+ *
+ * @author ${user}
+ */
+$(document).ready(function(){
+         
+});
+
+
