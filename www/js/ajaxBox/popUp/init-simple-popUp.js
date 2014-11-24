180c5e1d1fd7bbe186d4d861cf09077078ec7a21
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-11-24 12:59:15 +0100

    bug-fix pro více elementů na ajaxbox-popUp

--- www/js/ajaxBox/popUp/init-simple-popUp.js
+++ www/js/ajaxBox/popUp/init-simple-popUp.js
@@ -2,9 +2,13 @@
  * @copyright Copyright (c) 2013-2014 Kukral COMPANY s.r.o.
  */
 
-/* okénko s přihlašovacím/odhlašovacím menu */
-$('.ajaxbox-popUp').ajaxBox({
-	theme: "posPopUp",
-	autoPosition: 'center',
-	hideOthers: true
+/* ojednoduch0 vzskakovací okénko */
+$(document).ready(function() {
+	$('.ajaxbox-popUp').each(function() {
+		$(this).ajaxBox({
+			theme: "posPopUp",
+			autoPosition: 'center',
+			hideOthers: true
+		});
+	});
 });
