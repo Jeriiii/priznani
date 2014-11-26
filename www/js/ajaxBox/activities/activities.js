58a5b9f117a63ac76197ca59eb1cafc615f79364
JKoty <jan.kotalik@email.cz>
2014-11-26 14:51:01 +0100

    POS-488-bugfix-activity okénko

--- www/js/ajaxBox/activities/activities.js
+++ www/js/ajaxBox/activities/activities.js
@@ -2,12 +2,13 @@
  * @copyright Copyright (c) 2013-2014 Kukral COMPANY s.r.o.
  */
 
-var fnPreventNewActivity = function() {
-	$(".new-activity").each(function(){		
-		$(this).click(function(e) {
+var fnPreventNewActivity = function () {
+	$(".new-activity").each(function () {
+		$(this).click(function (e) {
 			e.preventDefault();
 			var $target = $(e.target);
-			var statusFn = function(data,status){}; //musim mu predat nejakou fnci
+			var statusFn = function (data, status) {
+			}; //musim mu predat nejakou fnci
 			/* oznaceni zpravy jako prectene */
 			$.get($target.data("activity-viewed-link"), statusFn);
 			/* prejdu na odkaz v tlacitku */
@@ -17,15 +18,15 @@ var fnPreventNewActivity = function() {
 };
 
 
-var reloadFn = function() {
+var reloadFn = function () {
 	fnPreventNewActivity();
 };
-	
+
 $('#activities').ajaxBox({
 	buttonSelector: '#activities-btn',
 	topMargin: -10, //korekce y
 	arrowOrientation: 'right', //šipka bude vpravo
-	theme: "posAjaxBox", //použijí se implicitní styly, ale budou upraveny
+	theme: "posAjaxBox interface", //použijí se implicitní styly, ale budou upraveny
 	headerHtml: "Aktivity", //header
 	streamSnippetModule: {
 		snippetName: 'snippet-activities-list',
