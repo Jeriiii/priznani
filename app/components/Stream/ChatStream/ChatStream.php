506f9226d855c634f73b33f26512ecdb093bd63a
Petr Kukrál <P.Kukral@seznam.cz>
2015-02-10 13:41:38 +0100

    založení komponenty na chat

---
+++ app/components/Stream/ChatStream/ChatStream.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * @copyright Copyright (c) 2013-2014 Kukral COMPANY s.r.o.
+ */
+
+/**
+ * Stream na velkých chat.
+ *
+ * @author Petr Kukrál
+ */
+
+namespace POSComponent\Stream;
+
+use Nette\ComponentModel\IContainer;
+use Nette\Database\Table\Selection;
+
+class ChatStream extends \POSComponent\BaseProjectControl implements \IStream {
+
+	/** @var Selection Zprávy co jsou v konverzaci - při předání v konstruktoru by měli být nevyfiltrované */
+	private $messages;
+
+	/** @var int Posun (zpráv) od konce konverzace. 0 = načti poslední zprávy z konverzace */
+	private $offset;
+
+	public function __construct(Selection $messages, IContainer $parent = NULL, $name = NULL) {
+		parent::__construct($parent, $name);
+		$this->messages = $messages;
+	}
+
+	public function render() {
+		$this->template->setFile(dirname(__FILE__) . '/chatStream.latte');
+		$this->template->render();
+	}
+
+	/**
+	 * Tuto metodu zavolejte ze metody render. Nastavý data, který se mají vrátit v ajaxovém i normálním
+	 * požadavku v závislosti na předaném offsetu (posunu od shora).
+	 * @param int $offset Offset předaný metodou handleGetMoreData. Při vyrendrování komponenty je nula.
+	 */
+	public function handleGetMoreData($offset, $limit) {
+		$this->offset = $offset;
+		$this->limit = $limit;
+		$this->setData($this->offset);
+
+		if ($this->presenter->isAjax()) {
+			$this->invalidateControl('stream-messages');
+		} else {
+			$this->redirect('this');
+		}
+	}
+
+	/**
+	 * Uloží předaný ofsset jako parametr třídy a invaliduje snippet s příspěvky
+	 * @param int $offset O kolik příspěvků se mám při načítání dalších příspěvků z DB posunout.
+	 */
+	public function setData($offset) {
+		$messages = $this->messages->limit($this->limit, $this->offset);
+		$this->template->messages = $messages;
+	}
+
+}
