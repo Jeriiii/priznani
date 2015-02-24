3826ae150bd01cf0efe55aa97aeb415ce5a8bffa
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-02-24 13:48:30 +0100

    Opravy chyby časů v DB scriptech

--- database/05-statistics.sql
+++ database/05-statistics.sql
@@ -2,9 +2,6 @@
 ALTER TABLE `stream_items`
 	ALTER `create` DROP DEFAULT;
 ALTER TABLE `stream_items`
-	CHANGE COLUMN `create` `create` DATETIME NOT NULL AFTER `type`;
-
-ALTER TABLE `stream_items`
 	CHANGE COLUMN `create` `create` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `type`;
 
 /* čas vytvoření uživatelských fotek */
