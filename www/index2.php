36ada7f07bf6ef4488196d399b1d8277372fc134
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-03-07 13:50:43 +0100

    přidání template na přesměrování na prozatimní adresu

--- www/index2.php
+++ www/index2.php
@@ -1,17 +1,1 @@
-<?php
-
-// absolute filesystem path to this web root
-define('WWW_DIR', __DIR__);
-
-// absolute filesystem path to the application root
-define('APP_DIR', WWW_DIR . '/../app2');
-
-// absolute filesystem path to the libraries
-define('LIBS_DIR', WWW_DIR . '/../vendor2/others/');
-
-// uncomment this line if you must temporarily take down your site for maintenance
-// require APP_DIR . '/templates/maintenance.phtml';
-// load bootstrap file
-$container = require APP_DIR . '/bootstrap.php';
-
-$container->application->run();
+<script>window.location.replace("http://42750.w50.wedos.ws/");</script>
