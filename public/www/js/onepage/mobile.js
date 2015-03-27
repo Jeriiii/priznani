f8474fd6bb9be730f5d8363a030b9dbea9dc1089
JKoty <jan.kotalik@email.cz>
2015-03-27 18:41:57 +0100

    Startovací skript pro mobilní homepage.

---
+++ public/www/js/onepage/mobile.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+	/* stream */
+	$("body").stream({
+		addoffset: 4
+	});
+	
+	/* zobrazování a schovávání komentářů nebo upozornění, že je uživatel nepřihlášený */
+	$('.comment-button, .likes-button').click(function(){
+		var selector = '#' + $(this).attr('data-to-open');
+		$(selector).toggle();
+	});
+});
+
