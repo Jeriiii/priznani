e7cb41ac20bc4a6ae00b40ae8b2ef3ca661a4aa4
JKoty <jan.kotalik@email.cz>
2015-03-27 16:14:13 +0100

    Aplikace jQuery mobile na části stránky donačtené přes snippet pomocí nette.ajax.

---
+++ public/www/js/onepage/nette.mobile.ext.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$.nette.ext('streamAjax', {
+		success: function () {
+			$('.stream-item').trigger("create");
+		}
+});
