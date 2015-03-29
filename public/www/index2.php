95578c69c7dd938134e549b374e3a611ea94a696
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-03-29 15:14:53 +0200

    další dodělávky na nahrání na server

---
+++ public/www/index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta name="Content-language" content="cs">
+		<meta charset="utf-8">
+		<title>Přiznání o sexu</title>
+	</head>
+
+	<body>
+		<strong>Vyčkejte pár minut, aktualizuje se systém. Děkujeme.</strong>
+	</body>
+</html>
+
+
