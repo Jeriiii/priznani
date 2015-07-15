f035fad46eedf7a3bb846b740cb11c8f4a206f40
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-07-15 14:50:57 +0200

    nahrání aktuální verze antispam controlu do formulářů

--- public/app/components/Antispam/AntispamControl.php
+++ public/app/components/Antispam/AntispamControl.php
@@ -4,16 +4,14 @@ use Nette\Forms\Controls\TextInput,
 	Nette\Forms\Form,
 	Nette\Utils\Html;
 
-
-
 /**
  * AntispamControl
- * add basic antispam feature to Nette forms. 
+ * add basic antispam feature to Nette forms.
  *
  * <code>
  * // Register extension
  * AntispamControl::register();
- * 
+ *
  * // Add antispam to form
  * $form->addAntispam();
  * </code>
@@ -22,39 +20,35 @@ use Nette\Forms\Controls\TextInput,
  * @author  Michal Mikoláš <nanuqcz@gmail.com>
  * @license CC BY <http://creativecommons.org/licenses/by/3.0/cz/>
  */
-class AntispamControl extends TextInput
-{
+class AntispamControl extends TextInput {
+
 	/** @var int  minimum delay [sec] to send form */
 	public static $minDelay = 5;
 
-	
 	/**
 	 * Register Antispam to Nette Forms
 	 * @return void
 	 */
-	public static function register()
-	{
-		Form::extensionMethod('addAntispam', function(Form $form, $name = 'spam', $label = 'Toto pole vymažte', $msg = 'Byl detekován pokus o spam.'){
+	public static function register() {
+		Form::extensionMethod('addAntispam', function(Form $form, $name = 'spam', $label = 'Toto pole vymažte', $msg = 'Byl detekován pokus o spam.') {
 			// "All filled" protection
 			$form[$name] = new AntispamControl($label, NULL, NULL, $msg);
-			
+
 			// "Send delay" protection
 			$form->addHidden('form_created', strtr(time(), '0123456789', 'jihgfedcba'))
 				->addRule(
-					function($item){
-						if (AntispamControl::$minDelay <= 0) return TRUE;  // turn off "Send delay protection"
+					function($item) {
+					if (AntispamControl::$minDelay <= 0)
+						return TRUE;  // turn off "Send delay protection"
 
-						$value = (int)strtr($item->value, 'jihgfedcba', '0123456789');
-						return $value <= (time() - AntispamControl::$minDelay);
-					}, 
-					$msg
-				);
-			
+					$value = (int) strtr($item->value, 'jihgfedcba', '0123456789');
+					return $value <= (time() - AntispamControl::$minDelay);
+				}, $msg
+			);
+
 			return $form;
 		});
 	}
-
-
 
 	/**
 	 * @param string|Html
@@ -62,41 +56,34 @@ class AntispamControl extends TextInput
 	 * @param int
 	 * @param string
 	 */
-	public function __construct($label = '', $cols = NULL, $maxLength = NULL, $msg = '')
-	{
+	public function __construct($label = '', $cols = NULL, $maxLength = NULL, $msg = '') {
 		parent::__construct($label, $cols, $maxLength);
 
 		$this->setDefaultValue('http://');
-		$this->addRule(~Form::FILLED, $msg);
+		$this->addRule(Form::BLANK, $msg);
 	}
-
-
 
 	/**
 	 * @return TextInput
 	 */
-	public function getControl()
-	{
+	public function getControl() {
 		$control = parent::getControl();
-		
+
 		$control = $this->addAntispamScript($control);
 		return $control;
 	}
-
-
 
 	/**
 	 * @param Html
 	 * @return Html
 	 */
-	protected function addAntispamScript(Html $control)
-	{
+	protected function addAntispamScript(Html $control) {
 		$control = Html::el('')->add($control);
-		$control->add( Html::el('script', array('type' => 'text/javascript'))->setHtml("
+		$control->add(Html::el('script', array('type' => 'text/javascript'))->setHtml("
 				// Clear input value
 				var input = document.getElementById('" . $control[0]->id . "');
-				input.value = '';				
-				
+				input.value = '';
+
 				// Hide input and label
 				if (input.parentNode.parentNode.nodeName == 'TR') {
 					// DefaultFormRenderer
@@ -111,7 +98,7 @@ class AntispamControl extends TextInput
 						}
 					}
 				}
-			") 
+			")
 		);
 
 		return $control;
