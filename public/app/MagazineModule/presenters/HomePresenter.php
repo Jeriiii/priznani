b8a91728cbf6da55454c245e7ee220210ccc3b6d
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-08-01 18:36:43 +0200

    přejmenování modulu, přidání javascriptů

--- public/app/MagazineModule/presenters/HomePresenter.php
+++ public/app/MagazineModule/presenters/HomePresenter.php
@@ -4,90 +4,90 @@
  *
  * @author Petr Kukrál <p.kukral@kukral.eu>
  */
-
 use Nette\Application\UI\Form as Frm,
 	\Michelf\Markdown;
 
-class DocumentationPresenter extends BasePresenter {
-	
+class HomePresenter extends BasePresenter {
+
 	public $page;
 	private $listPages;
-	
+
 	public function actionDefault($url = null) {
 		$this->loadPage($url);
 	}
-	
+
 	public function renderDefault($url = null) {
 		$convertor = new MarkdownToHtml();
-		
+
 		$this->template->name = $this->page->name;
 		$this->template->text = $convertor->toHtml($this->page->text);
 		$this->template->url = $this->page->url;
 		$this->template->listPages = !empty($this->listPages) ? $this->listPages : null;
 		$this->template->isHomepage = $this->page->homepage == 1 ? TRUE : FALSE;
 	}
-	
-	public function renderListPages() {		
+
+	public function renderListPages() {
 		$this->template->pages = $this->context->createPages()
-									->where("homepage", 0);
+			->where("homepage", 0);
 	}
-	
+
 	public function actionEditPage($url = null) {
 		$this->loadPage($url);
 	}
-	
+
 	public function actionNewPage() {
 		$this->page = $this->context->createPages()
-							->order("order DESC")
-							->fetch();
+			->order("order DESC")
+			->fetch();
 	}
-	
+
 	private function loadPage($url) {
-		if(empty($url)) {
+		if (empty($url)) {
 			/* homepage */
 			$page = $this->context->createPages()
-						->where("homepage", 1)
-						->fetch();
-			
+				->where("homepage", 1)
+				->fetch();
+
 			$this->listPages = $this->context->createPages()
-									->where("homepage", 0);
-			
-			if(! $this->getUser()->isAllowed("adminDocumentation")) {
+				->where("homepage", 0);
+
+			if (!$this->getUser()->isAllowed("adminDocumentation")) {
 				$this->listPages->where("access_rights", "all");
 			}
-				
+
 			$this->listPages->order("order ASC");
 		} else {
 			/* normal page */
 			$page = $this->context->createPages()
-						->where("url", $url)
-						->fetch();
-			
-			if(empty($page)) {
+				->where("url", $url)
+				->fetch();
+
+			if (empty($page)) {
 				$this->redirect("Error:404");
 			}
 		}
-		
-		if($page->access_rights != "all") {
+
+		if ($page->access_rights != "all") {
 			$this->isAdmin();
 		}
-		
+
 		$this->page = $page;
 	}
-	
+
 	public function handleDeletePage($idPage) {
 		$this->context->createPages()
 			->find($idPage)
 			->delete();
-		
+
 		$this->flashMessage('Stránka byla smazána.');
 	}
-	
+
 	protected function createComponentEditPageForm($name) {
 		return new Frm\editPageForm($this, $name);
 	}
-	
+
 	protected function createComponentNewPageForm($name) {
 		return new Frm\newPageForm($this, $name);
 	}
+
 }
---
+++ public/app/models/Dao/Magazine/Magazine.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * @copyright Copyright (c) 2013-2015 Kukral COMPANY s.r.o.
+ * created on 1.8.2015
+ */
+
+
+namespace POS\Model;
+
+/**
+ * NAME DAO NAMEDao
+ * slouží k
+ *
+ * @author Petr Kukrál <p.kukral@kukral.eu>
+ */
+class MagazinDao extends AbstractDao {
+
+	const TABLE_NAME = "";
+
+	/* Column name */
+	const COLUMN_ID = "id";
+
+	public function getTable() {
+		return $this->createSelection(self::TABLE_NAME);
+	}
+
+}
