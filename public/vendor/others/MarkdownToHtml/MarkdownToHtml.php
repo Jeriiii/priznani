4bdf0ba4a67ccc9299e8ebd65cfa8cb7c6cb1bf0
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-08-01 18:54:50 +0200

    vytvoření namespace u třídy MarkdownToHtml

--- public/vendor/others/MarkdownToHtml/MarkdownToHtml.php
+++ public/vendor/others/MarkdownToHtml/MarkdownToHtml.php
@@ -3,29 +3,27 @@
 # This file passes the content of the Readme.md file in the same directory
 # through the Markdown filter. You can adapt this sample code in any way
 # you like.
-
 # Install PSR-0-compatible class autoloader
 //spl_autoload_register(function($class){
 //	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
 //});
 
-# Get Markdown class
-use \Michelf\Markdown;
+namespace Michelf;
 
 # Read file and pass content through the Markdown parser
 
 class MarkdownToHtml {
-	
+
 	public function __construct() {
 
 	}
-	
+
 	public function toHtml($markdown) {
 		$html = Markdown::defaultTransform($markdown);
-		
+
 		return $html;
 	}
-}
 
+}
 
 ?>
