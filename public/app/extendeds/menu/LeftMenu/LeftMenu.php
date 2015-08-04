038865b8d56702b0a454b5df80341e443d427c99
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-08-04 21:08:32 +0200

    oprava přidávání param. v left menu

--- public/app/extendeds/menu/LeftMenu/LeftMenu.php
+++ public/app/extendeds/menu/LeftMenu/LeftMenu.php
@@ -13,13 +13,14 @@ use POSComponent\UsersList\FriendsList;
 use POSComponent\UsersList\SexyList\MarkedFromOther;
 use POSComponent\UsersList\BlokedUsersList;
 use NetteExt\Session\SessionManager;
+use POSComponent\BaseProjectControl;
 
 /**
  * Levé menu navigace na hlavní stránce a v mobilní verzi pak v layoutu
  *
  * @author Petr Kukrál <p.kukral@kukral.eu>
  */
-class LeftMenu extends \POSComponent\BaseProjectControl {
+class LeftMenu extends BaseProjectControl {
 
 	/** @var ArrayHash|ActiveRow Přihlášený uživatel. */
 	private $loggedUser;
@@ -70,11 +71,12 @@ class LeftMenu extends \POSComponent\BaseProjectControl {
 	private function createMenu() {
 		/* v mobilní verzi se pak zobrazuje v celém layoutu */
 		$menu = new Menu($this->presenter->user->isLoggedIn(), $this->showOn);
+		$p = $this->presenter;
 
-		$menu->addItem(new Item('Přihlášení', ':Sign:in'), false);
-		$menu->addItem(new Item('Registrace', ':Sign:registration'), false);
+		$menu->addItem(new Item('Přihlášení', $p->link(':Sign:in')), false);
+		$menu->addItem(new Item('Registrace', $p->link(':Sign:registration')), false);
 
-		$menu->addItem(new Item('Hledat přátele', ':Search:Search:', 'add-friend', array(
+		$menu->addItem(new Item('Hledat přátele', $p->link(':Search:Search:'), 'add-friend', array(
 			'data-step' => '4', 'data-position' => 'left', 'data-intro' =>
 			"Najdi nové přátele. Jednoduše jim pošli žádost o přátelství.", 'data-position' => "right")
 			)
@@ -89,24 +91,24 @@ class LeftMenu extends \POSComponent\BaseProjectControl {
 
 
 		$menu->addGroup(new Group('Profil'), true);
-		$menu->addItem(new Item('Editovat profil', ':Profil:Edit:default', 'edit'), true);
-		$menu->addItem(new Item('Můj profil', ':Profil:Show:default', 'profile'), true);
+		$menu->addItem(new Item('Editovat profil', $p->link(':Profil:Edit:default'), 'edit'), true);
+		$menu->addItem(new Item('Můj profil', $p->link(':Profil:Show:default'), 'profile'), true);
 		/* skrytí žádostí o ověření pro první verzi přiznání */
 		//$menu->addItem(new Item('<span>' . $countVerReqs . '</span>Žádostí o ověření', ':Profil:Show:verification', 'with-counter'), true);
 
 		$menu->addGroup(new Group('Obrázky'), true);
 		$menu->addItem(new Item('Nahrát fotky', null, 'add-gallery', array('id' => 'show-photo-form')), true, Menu::SHOW_ONE_PAGE);
-		$menu->addItem(new Item('Moje galerie', ':Profil:Galleries:default'), true);
+		$menu->addItem(new Item('Moje galerie', $p->link(':Profil:Galleries:default')), true);
 
 		if (isset($this->loggedUser) && $this->loggedUser->propertyID) { /* ochránit odkazy, které by spadly bez user properties */
 			$menu->addGroup(new Group('Uživatelé'));
 		}
 
 		$menu->addGroup(new Group('Zábava'));
-		$menu->addItem(new Item('Přiznání o sexu', 'OnePage: priznani => 1'), null);
-		$menu->addItem(new Item('Soutěže', ':Competition:list'), null);
-		$menu->addItem(new Item('Hry', ':Eshop:game'), null);
-		$menu->addItem(new Item('Bonusy', ':Page:metro'), null);
+		$menu->addItem(new Item('Přiznání o sexu', $p->link('OnePage:', array('priznani' => 1))), null);
+		$menu->addItem(new Item('Soutěže', $p->link(':Competition:list')), null);
+		$menu->addItem(new Item('Hry', $p->link(':Eshop:game')), null);
+		$menu->addItem(new Item('Bonusy', $p->link(':Page:metro')), null);
 
 		return $menu;
 	}
