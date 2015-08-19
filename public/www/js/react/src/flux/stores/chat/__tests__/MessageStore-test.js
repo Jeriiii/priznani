fed6be6270f161218a91597face8ec55dde68dbf
JKoty <jan.kotalik@email.cz>
2015-08-19 17:59:28 +0200

    vytvoření způsobu testování akcí

--- public/www/js/react/src/flux/stores/chat/__tests__/MessageStore-test.js
+++ public/www/js/react/src/flux/stores/chat/__tests__/MessageStore-test.js
@@ -4,20 +4,25 @@
  */
 
 /* global React *//* aby Netbeans nevyhazoval chyby kvůli nedeklarované proměnné */
+
 jest.dontMock('../MessageStore');
 jest.dontMock('object-assign');
+jest.autoMockOff();
+var ActionTypes = require('../../../constants/ActionConstants');
+jest.autoMockOn();
 
+
+//https://github.com/facebook/jest/issues/17
 describe('ChatMessageStore', function() {
+
 
   var Dispatcher;
   var MessageStore;
-  var ActionTypes;
   var callback;
 
   beforeEach(function() {
     Dispatcher = require('../../../dispatcher/datenodeDispatcher');
     MessageStore = require('../MessageStore');
-    ActionTypes = require('../../../constants/ActionConstants').ActionTypess;
     callback = Dispatcher.register.mock.calls[0][0];
   });
 
@@ -28,11 +33,30 @@ describe('ChatMessageStore', function() {
 
   /* ověření, že když nepřijdou zprávy, tak se žádné nepřidají */
   it('adds no message into its data when there is not any', function() {
-    // var action = {
-    //   type: ActionTypess.test
-    // };
-    // callback(action);
-    // expect(MessageStore.getState().messages.length).toBe(0);
+    var action = {
+      type: ActionTypes.NO_INITIAL_MESSAGES_ARRIVED
+    };
+    callback(action);
+    expect(MessageStore.getState().messages.length).toBe(0);
+  });
+
+  /* ověření, že se přidají donačtené zprávy */
+  it('adds older messages', function() {
+    var action = {
+      type: 'OLDER_MESSAGES_ARRIVED',
+      userCodedId: "111",
+      usualMessagesCount : 10,
+      data: {'111': {
+        messages: [
+          {id: 1, text: "testtext1"},
+          {id: 2, text: "testtext2"},
+          {id: 3, text: "testtext3"},
+          {id: 4, text: "testtext4"}
+        ]
+      }}
+    };
+    callback(action);
+    expect(MessageStore.getState().messages.length).toBe(4);
   });
 
 });
