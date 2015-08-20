190636d5adbf688cfacd0e048387646995380738
JKoty <jan.kotalik@email.cz>
2015-08-20 10:07:53 +0200

    Testovací případy.

--- public/www/js/react/src/flux/stores/chat/__tests__/MessageStore-test.js
+++ public/www/js/react/src/flux/stores/chat/__tests__/MessageStore-test.js
@@ -59,4 +59,62 @@ describe('ChatMessageStore', function() {
     expect(MessageStore.getState().messages.length).toBe(4);
   });
 
+  /* ověření, že se přidá o jednu zprávu míň, když jich je dostatek */
+  it('does not add all messages when there is enaugh messages', function() {
+    var action = {
+      type: 'OLDER_MESSAGES_ARRIVED',
+      userCodedId: "111",
+      usualMessagesCount : 3,
+      data: {'111': {
+        messages: [
+          {id: 1, text: "testtext1"},
+          {id: 2, text: "testtext2"},
+          {id: 3, text: "testtext3"},
+          {id: 4, text: "testtext4"}
+        ]
+      }}
+    };
+    callback(action);
+    expect(MessageStore.getState().messages.length).toBe(3);
+  });
+
+  /* oveření, že se první zpráva přidá na začátek */
+  it('adds messages in the right order', function() {
+    var action = {
+      type: 'OLDER_MESSAGES_ARRIVED',
+      userCodedId: "111",
+      usualMessagesCount : 10,
+      data: {'111': {
+        messages: [
+          {id: 1, text: "testtext1"},
+          {id: 2, text: "testtext2"},
+          {id: 3, text: "testtext3"},
+          {id: 4, text: "testtext4"}
+        ]
+      }}
+    };
+    callback(action);
+    expect(MessageStore.getState().messages[0].text).toBe("testtext1");
+  });
+
+  /* oveření, že je možné přidat nové zprávy na konec */
+  it('adds messages at the end', function() {
+    var action = {
+      type: 'NEW_MESSAGES_ARRIVED',
+      userCodedId: "111",
+      data: {'111': {
+        messages: [
+          {id: 100, text: "testtext1"},
+          {id: 200, text: "testtext2"},
+          {id: 300, text: "testtext3"},
+          {id: 400, text: "testtext4"}
+        ]
+      }}
+    };
+    callback(action);
+    var messages = MessageStore.getState().messages;
+    expect(messages.length).toBe(4);
+    expect(messages[messages.length - 1].text).toBe("testtext4");
+  });
+
 });
