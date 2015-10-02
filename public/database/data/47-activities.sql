73317d86f47e1730315b7a46f9ea2e295809cdcf
Petr Kukrál <p.kukral@jkbusiness.cz>
2015-10-02 10:14:57 +0200

    fast-bug-fix přidání dat do db, aby procházeli testy

---
+++ public/database/data/47-activities.sql
@@ -0,0 +1,9 @@
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (1, NULL, NULL, 5, NULL, NULL, 4, 3, 1, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (2, NULL, NULL, 4, NULL, NULL, 4, 8, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (3, NULL, 4, NULL, NULL, NULL, 4, 7, 1, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (4, 'addNew', NULL, 6, NULL, NULL, NULL, 4, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (5, 'addNew', 7, NULL, NULL, NULL, NULL, 4, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (6, 'addNew', NULL, 7, NULL, NULL, NULL, 4, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (7, 'addNew', 7, NULL, NULL, NULL, NULL, 4, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (8, 'addNew', 7, NULL, NULL, NULL, NULL, 4, 0, 0);
+INSERT INTO `activities` (`id`, `type`, `imageID`, `statusID`, `commentImageID`, `friendRequestID`, `event_ownerID`, `event_creatorID`, `viewed`, `sendNotify`) VALUES (9, 'addNew', 7, NULL, NULL, NULL, NULL, 4, 0, 0);
